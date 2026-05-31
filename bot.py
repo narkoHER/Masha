51d3f7d0336efdaa436e81aa2a2977c3c31ba839
narkoHER <152059061+narkoHER@users.noreply.github.com>
2026-06-01 01:58:42 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import time
 
 BOT_TOKEN   = "8566954882:AAHFX769zpn0d_CpQhEnM9V_Isv7TUwnT7s"
 CHANNEL_ID  = "-1003758993082"
-MINIAPP_URL = "t.me/masha_durak_bot/httpsnarkohergithubiomasha"
-API         = f"https://api.telegram.org/bot{8566954882:AAHFX769zpn0d_CpQhEnM9V_Isv7TUwnT7s}"
+MINIAPP_URL = "https://narkoher.github.io/Masha/"
+API         = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 r = requests.post(f"{API}/deleteWebhook", json={"drop_pending_updates": True})
 print("deleteWebhook:", r.json())
